ac970178b71185df29c8e507ca8221e3025920da
Mikael Wallin <mikael.x.wallin@gmail.com>
2022-09-11 23:37:14 +0200

    Two working supertests

--- __tests__/supertest.js
+++ __tests__/supertest.js
@@ -19,7 +19,56 @@ const pet = {
 	"status": "available"
   }
 
+describe('When testing jest', () => {
+	describe('given i have a non failing test', () => {
+		it('should be one', () => {
+			expect(1).toBe(1)
+		})
+	})
 
-it('Testing to see if Jest works', () => {
-	expect(1).toBe(1)
+	describe('given i have a non failing test', () => {
+		it('should be two', () => {
+			expect(2).toBe(2)
+		})
+	})
+})
+
+describe('When testing jest', () => {
+	describe('given i have a non failing test', () => {
+		it.skip('should fail', () => {
+			expect(1).toBe(2)
+		})
+	})
+})
+
+describe("Testing petshop", () => {
+	describe('given a broken url', () => {
+		it('should return status 404', () => {
+			const container = request("https://petstore.swagger.io/")
+			container.get('/whatever')
+				.expect(404)
+		})
+	})
+})
+
+describe("Testing petshop", () => {
+	describe('given GET /', () => {
+		it('should return status 200', () => {
+			container = request("https://petstore.swagger.io/")
+			container.get('/')
+				.expect('Allow', /GET/)
+				.expect(200);
+		})
+	})
+})
+
+describe("Testing petshop", () => {
+	describe('given POST /v2/pet', () => {
+		it('should return status 200', () => {
+			container = request("https://petstore.swagger.io/")
+			container.post('/v2/pet')
+				.expect('Allow', /POST/)
+				.expect(200);
+		})
+	})
 })
